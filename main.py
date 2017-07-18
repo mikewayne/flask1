538776b027dd7eab6e5d1813f2743f9764f9676c
Michael Hoeft <mwhoeft@gmail.com>
2017-07-18 13:39:39 -0700

    iniitial commit

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+form = """
+
+<!DOCTYPE html>
+<html lang="en-US">
+    <body>
+        <form action="/hello">
+            <label for="first-name">First Name:</label>
+            <input id="first-name" type="text" name="first_name"/>
+            <input type="submit"/>
+        </form>
+    </body>
+</html>    
+"""
+
 @app.route("/")
 def index():
-    return "Hello World"
+    return form
+
+@app.route("/hello")
+def hello():
+    first_name = request.args.get("first_name")
+    return "Hello, " + first_name + ". Enjoy your stay."   
 
 app.run()    
